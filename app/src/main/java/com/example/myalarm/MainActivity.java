0f3b6ec9358d7fe5ef14a6bae08df89db73cd344
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-11-02 13:09:35 +0600

    activity main

--- app/src/main/java/com/example/myalarm/MainActivity.java
+++ app/src/main/java/com/example/myalarm/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.myalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.os.Build;
 import android.os.Bundle;
 
+import com.example.myalarm.databinding.ActivityMainBinding;
+import com.google.android.material.timepicker.MaterialTimePicker;
+
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private MaterialTimePicker timePicker;
+    private Calendar calender;
+    private AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void createNotificationChanned(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            CharSequence name="akchannel";
+            String desc="Channel for Alarm Manager";
+            int imp= NotificationManager.IMPORTANCE_HIGH;
+
+        }
     }
 }
